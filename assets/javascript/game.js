aa2db7d5256c523342123a9e680cee9fb3f780c6
Bridgette Smith <bsmith@ift.org>
2018-03-02 16:51:13 -0600

    3rd commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Create variables for the guesses
+
+let singer1 = "Beyonce";
+let singer2 = "Bruno Mars";
+let singer3 = "Michael Jackson";
+let singer4 = "Prince";
+let singer5 = "Shakira";
+let dashGuess = [];
+
+// Dash display for Singer 1: nBeyonce
+for (var i = 0; i < singer1.length; i++) {
+    dashGUess.push("_");
+}
+
